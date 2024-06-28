b6a612ee09bc746878107792c3ddca58b0fb94e8
Kian_Bahasadri <101868619+KianBahasadri@users.noreply.github.com>
2024-06-28 15:18:35 -0400

    factorization attack

--- rsa_utils.py
+++ rsa_utils.py
@@ -1,5 +1,19 @@
-# this script might be useful when imported into an interactive terminal
 from math import lcm, gcd
+def print_function_list():
+  print("""-----------------------------------------------------------------
+encrypt(m, e, n): Encrypts a message.
+          Params:   m: str   e: int   n: int
+          Returns: int
+decrypt(m, d, n): Decrypts a message.
+          Params:   m: int   d: int   n: int
+          Returns: str
+get_edn_from_pq(p, q): Generates e, d, n.
+          Params:   p: int   q: int
+          Returns: tuple
+nth_int_root(x, n): Calculates nth root.
+          Params:   x: int   n: int
+          Returns: int
+-----------------------------------------------------------------""")
 
 def encrypt(m, e, n):
   number = int.from_bytes(m.encode('utf-8'))
@@ -39,9 +53,24 @@ def nth_int_root(x, n):
 
 def print_attack_list():
   print("""-----------------------------------------------------------------
-  attack1(M, e, n, _range, plaintext) - when m ** e is not much bigger than n
-      - this may happen if the plaintext is short, lacks padding, and e is small
-  attack1_multithread(M, e, n, _range, plaintext) -- multithreaded attack1()
+attack1(M, e, n, _range, plaintext):
+        Performs an attack on RSA encryption when m ** e is not much bigger than n.
+        This may happen if the plaintext is short, lacks padding, and e is small.
+          M (int, optional): The integer message. Default is False (will prompt for input).
+          e (int, optional): The public exponent. Default is False (will prompt for input).
+          n (int, optional): The modulus. Default is False (will prompt for input).
+          _range (int, optional): The number of iterations for the attack. Default is False (will prompt for input).
+          plaintext (str, optional): The plaintext message. Default is False (will prompt for input).
+          Returns:
+              None
+attack2(n):
+        Attempts to factor the given integer n
+        Uses factordb and then attempts to manually factor
+        Warning: kind of shit
+factorDB(n):
+        requests the factorization from http://factordb.com/api
+        checks if its valid and returns it if it is
+
 -----------------------------------------------------------------""")
 
 def attack1(M=False, e=False, n=False, _range=False, plaintext=False):
@@ -66,5 +95,30 @@ def attack1(M=False, e=False, n=False, _range=False, plaintext=False):
     elif i % 1000 == 0:
       print(i)
       #print(dec[:32])
-    
 
+def attack2(n):
+  db = factorDB(n)
+  if db:
+    print(db)
+  else:
+    if n > 10**20:
+      print("WARNING: if n is made up of large primes this is gonna take a long time")
+      print("Reccomend to use https://gitlab.inria.fr/cado-nfs instead")
+    print("Factor DB failed factorization, continuing manually")
+    from sympy import factorint
+    print(factorint(n))
+
+def factorDB(n):
+  import json
+  import requests
+  response = requests.get(f"http://factordb.com/api?query={n}")
+  datadict = json.loads(response.content)
+  if datadict['status'] in ('C', 'U', 'C*', 'U*'):
+    return None
+  else:
+    return datadict['factors']
+
+
+#-----------------------------------------------------------------
+print_function_list()
+print_attack_list()
