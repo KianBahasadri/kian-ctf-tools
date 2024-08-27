f1cb64354a5298114a49fe1b3b6c52ddd6fad02e
Kian_Bahasadri <101868619+KianBahasadri@users.noreply.github.com>
2024-08-27 15:29:08 -0400

    stuff

--- locate_ssh_host.sh
+++ locate_ssh_host.sh
@@ -3,29 +3,56 @@
 
 set -e 
 
-if [[ $# != 2 ]]
+if [[ $# != 3 ]]
 then
-  echo 'Usage ./script <username> <path_to_keyfile>'
+  echo 'Usage ./script <username> "password"|"keyfile" <path_to_keyfile|password>'
   exit
 fi
 
 username=$1
-keyfile=$2
-echo username: "$username"
-echo keyfile: "$keyfile"
-echo looking in 192.168.0.0/24
 
-for ip in 192.168.0.{0..255}
-do
-  ssh "$username"@$ip \
-    -q \
-    -i "$keyfile" \
-    -o StrictHostKeyChecking=no \
-    -o UserKnownHostsFile=/dev/null \
-    -o ConnectTimeout=1 \
-    -o BatchMode=true \
-    echo Successfull SSH connection at $ip &
-done
+if [[ "$2" == password ]]
+then
+  password="$3"
+  echo username: "$username"
+  echo password: "$password"
+  echo looking in 192.168.0.0/24
 
-sleep 2
+  for ip in 192.168.0.{0..255}
+  do
+    sshpass -p"$password" \
+    ssh "$username"@$ip \
+      -q \
+      -o StrictHostKeyChecking=no \
+      -o UserKnownHostsFile=/dev/null \
+      -o ConnectTimeout=1 \
+      echo SSH connection at $ip, hostname '"$(hostname)"' &
+    done
+    sleep 3
+
+elif [[ "$2" == keyfile ]]
+then
+  keyfile="$3"
+  echo username: "$username"
+  echo keyfile: "$keyfile"
+  echo looking in 192.168.0.0/24
+
+  for ip in 192.168.0.{0..255}
+  do
+    ssh "$username"@$ip \
+      -q \
+      -i "$keyfile" \
+      -o StrictHostKeyChecking=no \
+      -o UserKnownHostsFile=/dev/null \
+      -o ConnectTimeout=1 \
+      -o BatchMode=true \
+      echo SSH connection at $ip, hostname '"$(hostname)"' &
+  done
+  sleep 3
+
+else
+  echo 'Second argument must be "password" or "keyfile"'
+  exit
+fi
+
 
